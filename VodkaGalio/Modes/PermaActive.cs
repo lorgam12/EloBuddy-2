640853554ffe11a3b7a68629476eed4d4387a356
Tomasz Świerkot <tomek.swierkot@gmail.com>
2015-11-15 17:19:10 +0100

    Reorganized PermaActive

--- VodkaGalio/Modes/PermaActive.cs
+++ VodkaGalio/Modes/PermaActive.cs
@@ -34,112 +34,121 @@ namespace VodkaGalio.Modes
                 return;
             }
             // KillSteal
-            foreach (var enemy in EntityManager.Heroes.Enemies.Where(e => e.IsEnemy && e.IsVisible && !e.IsDead && !e.IsZombie && !e.IsInvulnerable && e.Health > 0))
+            var enemies = EntityManager.Heroes.Enemies.Where(e => e.IsEnemy && e.IsVisible && !e.IsDead && !e.IsZombie && !e.IsInvulnerable && e.Health > 0).ToList();
+            if (!isUlting())
             {
-                if (!isUlting() && Settings.KsQ && SpellManager.Q.IsReady() && Damages.QDamage(enemy) > enemy.Health && SpellManager.Q.IsInRange(enemy))
+                foreach (var enemy in enemies)
                 {
-                    if (enemy.HasBuffOfType(BuffType.SpellImmunity) || enemy.HasBuffOfType(BuffType.SpellShield))
+                    if (Settings.KsQ && SpellManager.Q.IsReady() && Damages.QDamage(enemy) > enemy.Health &&
+                        SpellManager.Q.IsInRange(enemy))
                     {
-                        continue;
+                        if (enemy.HasBuffOfType(BuffType.SpellImmunity) || enemy.HasBuffOfType(BuffType.SpellShield))
+                        {
+                            continue;
+                        }
+                        var pred = Q.GetPrediction(enemy);
+                        if (pred.HitChance >= HitChance.High)
+                        {
+                            Debug.WriteChat("Casting Q in KillSteal on {0}, who has {1} HP", enemy.ChampionName,
+                                "" + enemy.Health);
+                            Q.Cast(pred.CastPosition);
+                            break;
+                        }
                     }
-                    var pred = Q.GetPrediction(enemy);
-                    if (pred.HitChance >= HitChance.High)
+
+                    if (Settings.KsE && SpellManager.E.IsReady() && Damages.EDamage(enemy) > enemy.Health &&
+                        SpellManager.E.IsInRange(enemy))
                     {
-                        Debug.WriteChat("Casting Q in KillSteal on {0}, who has {1} HP", enemy.ChampionName, ""+enemy.Health);
-                        Q.Cast(pred.CastPosition);
+                        if (enemy.HasBuffOfType(BuffType.SpellImmunity) || enemy.HasBuffOfType(BuffType.SpellShield))
+                        {
+                            continue;
+                        }
+                        var pred = E.GetPrediction(enemy);
+                        if (pred.HitChance >= HitChance.High)
+                        {
+                            Debug.WriteChat("Casting E in KillSteal on {0}, who has {1} HP", enemy.ChampionName,
+                                "" + enemy.Health);
+                            E.Cast(pred.CastPosition);
+                            break;
+                        }
+                    }
+
+                    if (SpellManager.Ignite != null && Settings.KsIgnite && SpellManager.Ignite.IsReady() &&
+                        Damages.IgniteDmg(enemy) > enemy.Health && SpellManager.Ignite.IsInRange(enemy))
+                    {
+                        Debug.WriteChat("Casting Ignite in KillSteal on {0}, who has {1} HP", enemy.ChampionName,
+                            "" + enemy.Health);
+                        SpellManager.Ignite.Cast(enemy);
                         break;
                     }
-                }
 
-                if (!isUlting() && Settings.KsE && SpellManager.E.IsReady() && Damages.EDamage(enemy) > enemy.Health && SpellManager.E.IsInRange(enemy))
-                {
-                    if (enemy.HasBuffOfType(BuffType.SpellImmunity) || enemy.HasBuffOfType(BuffType.SpellShield))
+                    if (Settings.KsR && SpellManager.R.IsReady() && Damages.RDamage(enemy) > enemy.Health &&
+                        SpellManager.R.IsInRange(enemy))
                     {
-                        continue;
-                    }
-                    var pred = E.GetPrediction(enemy);
-                    if (pred.HitChance >= HitChance.High)
-                    {
-                        Debug.WriteChat("Casting E in KillSteal on {0}, who has {1} HP", enemy.ChampionName, "" + enemy.Health);
-                        E.Cast(pred.CastPosition);
+                        if (enemy.HasBuffOfType(BuffType.SpellImmunity) || enemy.HasBuffOfType(BuffType.SpellShield))
+                        {
+                            continue;
+                        }
+                        Debug.WriteChat("Casting R in KillSteal on {0}, who has {1} HP", enemy.ChampionName,
+                            "" + enemy.Health);
+                        R.Cast();
                         break;
                     }
-                }
-
-                if (!isUlting() && SpellManager.Ignite != null && Settings.KsIgnite && SpellManager.Ignite.IsReady() &&
-                    Damages.IgniteDmg(enemy) > enemy.Health && SpellManager.Ignite.IsInRange(enemy))
-                {
-                    Debug.WriteChat("Casting Ignite in KillSteal on {0}, who has {1} HP", enemy.ChampionName, "" + enemy.Health);
-                    SpellManager.Ignite.Cast(enemy);
-                    break;
-                }
-
-                if (!isUlting() && Settings.KsR && SpellManager.R.IsReady() && Damages.RDamage(enemy) > enemy.Health && SpellManager.R.IsInRange(enemy))
-                {
-                    if (enemy.HasBuffOfType(BuffType.SpellImmunity) || enemy.HasBuffOfType(BuffType.SpellShield))
-                    {
-                        continue;
-                    }
-                    Debug.WriteChat("Casting R in KillSteal on {0}, who has {1} HP", enemy.ChampionName, "" + enemy.Health);
-                    R.Cast();
-                    break;
                 }
             }
 
             // Automatic ult usage
             if (Settings.AutoR && R.IsReady() && !Player.Instance.IsRecalling())
             {
-                var enemies = EntityManager.Heroes.Enemies.Where(e => !e.IsDead && !e.IsRecalling() && !e.IsZombie && !e.IsInvulnerable && R.IsInRange(e)).ToList();
-                if (enemies.Count() >= Settings.AutoRMinEnemies && Player.Instance.HealthPercent >= Settings.AutoRMinHP)
+                var ultableEnemies = enemies.Where(e => R.IsInRange(e)).ToList();
+                if (ultableEnemies.Count() >= Settings.AutoRMinEnemies && Player.Instance.HealthPercent >= Settings.AutoRMinHP)
                 {
                     if (W.IsReady() && Player.Instance.Mana >= 160)
                     {
                         W.Cast(Player.Instance);
                     }
-                    Debug.WriteChat("AutoCasting R, Enemies in range: {0}", ""+""+enemies.Count());
+                    Debug.WriteChat("AutoCasting R, Enemies in range: {0}", "" + "" + ultableEnemies.Count());
                     R.Cast();
                     return;
                 }
             }
 
             // Potion manager
-            if (Settings.Potion && !Player.Instance.IsInShopRange())
-            {
-                if (Player.Instance.HealthPercent <= Settings.potionMinHP && !(Player.Instance.HasBuff("RegenerationPotion") || Player.Instance.HasBuff("ItemMiniRegenPotion") || Player.Instance.HasBuff("ItemCrystalFlask") || Player.Instance.HasBuff("ItemDarkCrystalFlask")))
+
+            if (Settings.Potion && !Player.Instance.IsInShopRange() && Player.Instance.HealthPercent <= Settings.potionMinHP && !(Player.Instance.HasBuff("RegenerationPotion") || Player.Instance.HasBuff("ItemMiniRegenPotion") || Player.Instance.HasBuff("ItemCrystalFlask") || Player.Instance.HasBuff("ItemDarkCrystalFlask")))
                 {
-                    if (Item.HasItem(HealthPotion.Id) && Item.CanUseItem(HealthPotion.Id))
-                    {
-                        Debug.WriteChat("Using HealthPotion because below {0}% HP - have {1}% HP", ""+Settings.potionMinHP, String.Format("{0:##.##}", Player.Instance.HealthPercent));
-                        HealthPotion.Cast();
-                        return;
-                    }
-                    if (Item.HasItem(TotalBiscuit.Id) && Item.CanUseItem(TotalBiscuit.Id))
-                    {
-                        Debug.WriteChat("Using TotalBiscuitOfRejuvenation because below {0}% HP - have {1}% HP", "" + Settings.potionMinHP, String.Format("{0:##.##}", Player.Instance.HealthPercent));
-                        TotalBiscuit.Cast();
-                        return;
-                    }
-                    if (Item.HasItem(RefillablePotion.Id) && Item.CanUseItem(RefillablePotion.Id))
-                    {
-                        Debug.WriteChat("Using RefillablePotion because below {0}% HP - have {1}% HP", "" + Settings.potionMinHP, "" + String.Format("{0:##.##}", Player.Instance.HealthPercent));
-                        RefillablePotion.Cast();
-                        return;
-                    }
-                    if (Item.HasItem(CorruptingPotion.Id) && Item.CanUseItem(CorruptingPotion.Id))
-                    {
-                        Debug.WriteChat("Using CorruptingPotion because below {0}% HP - have {1}% HP", "" + Settings.potionMinHP, "" + String.Format("{0:##.##}", Player.Instance.HealthPercent));
-                        CorruptingPotion.Cast();
-                        return;
-                    }
+                if (Item.HasItem(HealthPotion.Id) && Item.CanUseItem(HealthPotion.Id))
+                {
+                    Debug.WriteChat("Using HealthPotion because below {0}% HP - have {1}% HP", "" + Settings.potionMinHP, String.Format("{0:##.##}", Player.Instance.HealthPercent));
+                    HealthPotion.Cast();
+                    return;
                 }
-                if (Player.Instance.ManaPercent <= Settings.potionMinMP && !(Player.Instance.HasBuff("RegenerationPotion") || Player.Instance.HasBuff("ItemMiniRegenPotion") || Player.Instance.HasBuff("ItemCrystalFlask") || Player.Instance.HasBuff("ItemDarkCrystalFlask")))
+                if (Item.HasItem(TotalBiscuit.Id) && Item.CanUseItem(TotalBiscuit.Id))
                 {
-                    if (Item.HasItem(CorruptingPotion.Id) && Item.CanUseItem(CorruptingPotion.Id))
-                    {
-                        Debug.WriteChat("Using HealthPotion because below {0}% MP - have {1}% MP", "" + Settings.potionMinMP, "" + String.Format("{0:##.##}", Player.Instance.ManaPercent));
-                        CorruptingPotion.Cast();
-                        return;
-                    }
+                    Debug.WriteChat("Using TotalBiscuitOfRejuvenation because below {0}% HP - have {1}% HP", "" + Settings.potionMinHP, String.Format("{0:##.##}", Player.Instance.HealthPercent));
+                    TotalBiscuit.Cast();
+                    return;
+                }
+                if (Item.HasItem(RefillablePotion.Id) && Item.CanUseItem(RefillablePotion.Id))
+                {
+                    Debug.WriteChat("Using RefillablePotion because below {0}% HP - have {1}% HP", "" + Settings.potionMinHP, "" + String.Format("{0:##.##}", Player.Instance.HealthPercent));
+                    RefillablePotion.Cast();
+                    return;
+                }
+                if (Item.HasItem(CorruptingPotion.Id) && Item.CanUseItem(CorruptingPotion.Id))
+                {
+                    Debug.WriteChat("Using CorruptingPotion because below {0}% HP - have {1}% HP", "" + Settings.potionMinHP, "" + String.Format("{0:##.##}", Player.Instance.HealthPercent));
+                    CorruptingPotion.Cast();
+                    return;
+                }
+            }
+            if (Player.Instance.ManaPercent <= Settings.potionMinMP && !(Player.Instance.HasBuff("RegenerationPotion") || Player.Instance.HasBuff("ItemMiniRegenPotion") || Player.Instance.HasBuff("ItemCrystalFlask") || Player.Instance.HasBuff("ItemDarkCrystalFlask")))
+            {
+                if (Item.HasItem(CorruptingPotion.Id) && Item.CanUseItem(CorruptingPotion.Id))
+                {
+                    Debug.WriteChat("Using HealthPotion because below {0}% MP - have {1}% MP", "" + Settings.potionMinMP, "" + String.Format("{0:##.##}", Player.Instance.ManaPercent));
+                    CorruptingPotion.Cast();
+                    return;
                 }
             }
         }
