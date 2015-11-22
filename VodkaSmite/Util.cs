15a2a7efebd8bd0a5222e584007a537a86e3c1a3
Tomasz Świerkot <tomek.swierkot@gmail.com>
2015-11-22 23:55:45 +0100

    VodkaSmite: Initial release

---
+++ VodkaSmite/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VodkaSmite
+{
+    class Util
+    {
+        public readonly static string[] MonstersNames =
+        {
+            "TT_Spiderboss", "TTNGolem", "TTNWolf", "TTNWraith",
+            "SRU_Blue", "SRU_Gromp", "SRU_Murkwolf", "SRU_Razorbeak",
+            "SRU_Red", "SRU_Krug", "SRU_Dragon", "Sru_Crab", "SRU_Baron"
+        };
+
+        public static readonly string[] SmiteNames =
+        {
+            "summonersmite", "s5_summonersmiteplayerganker", "s5_summonersmiteduel"
+        };
+    }
+}
