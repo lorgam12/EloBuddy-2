3a9bcac99223d6e1f3aad2d94ff0d7790e1d67a6
Tomasz Świerkot <tomek.swierkot@gmail.com>
2016-05-09 11:07:59 +0200

    UPDATE VodkaAzir Added a note saying this addon is not currently supported.

--- VodkaAzir/Program.cs
+++ VodkaAzir/Program.cs
@@ -31,6 +31,9 @@ namespace VodkaAzir
         private static void WelcomeMsg()
         {
             Chat.Print("Vodka{0} Loaded. Have a splendid game!", Color.LightGoldenrodYellow, ChampName);
+            Chat.Print("===============================", Color.Red);
+            Chat.Print("THIS ADDON IS NOT CURRENTLY SUPPORTED. IT HAS NOT BEEN UPDATED SINCE 5.24 (December 2015). Use at your own risk.");
+            Chat.Print("===============================", Color.Red);
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("Vodka{0} Loaded. Have a splendid game!", ChampName);
             Console.ResetColor();
